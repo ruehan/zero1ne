063410c49d830b195a5f3c2f7f64d66991cf3012
GYU HAN <ruehan98@gmail.com>
2025-03-29 19:02:17 +0900

    fix : 맵 제거

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { motion, useAnimation, useInView } from "framer-motion";
-import MapComponent from "./MapComponent";
+// import MapComponent from "./MapComponent";
 import ContactModal from "../Modal/ContactModal";
 
 // 스타일드 컴포넌트
@@ -292,7 +292,7 @@ const Contact: React.FC = () => {
 						</ContactInfo>
 
 						<MapWrapper initial="hidden" animate={controls} variants={rightAnimation}>
-							<MapComponent position={position} popupText="(주)제로원 <br /> 충청북도 청주시 청원구 오창읍 양청송대길 10, 308호" />
+							{/* <MapComponent position={position} popupText="(주)제로원 <br /> 충청북도 청주시 청원구 오창읍 양청송대길 10, 308호" /> */}
 						</MapWrapper>
 					</ContactContent>
 				</ContactInner>
